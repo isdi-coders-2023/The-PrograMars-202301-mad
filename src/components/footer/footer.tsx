92557ac101e126848b0902846aa53507eb6bdb46
sfernandez <sfdezlop@gmail.com>
2023-02-13 20:26:25 +0100

    Update component footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,49 +4,39 @@ export function Footer() {
     <footer className="footer">
       <ul>
         <li>
-          <div>
-            <a rel="nofollow" href="https://twitter.com/ISDICoders">
-              <img
-                src="assets/img/logo-twitter.svg"
-                alt="Logo to link to the ISDI Coders's Twitter page"
-              ></img>
-            </a>
-          </div>
+          <a rel="nofollow" href="https://twitter.com/ISDICoders">
+            <img
+              src="assets/img/logo-twitter.svg"
+              alt="Logo to link to the ISDI Coders's Twitter page"
+            ></img>
+          </a>
         </li>
         <li>
-          <div>
-            <a rel="nofollow" href="https://isdicoders.com/">
-              <img
-                src="assets/img/logo-ISDI.svg"
-                alt="Logo to link to the ISDI Coders's Twitter page"
-              ></img>
-            </a>
-          </div>
+          <a rel="nofollow" href="https://isdicoders.com/">
+            <img
+              src="assets/img/logo-ISDI.svg"
+              alt="Logo to link to the ISDI Coders's Twitter page"
+            ></img>
+          </a>
         </li>
         <li>
-          <div>
-            <p>ISDI coders 2023 - NASA API</p>
-          </div>
+          <p>ISDI coders 2023 - NASA API</p>
         </li>
         <li>
-          <div>
-            <a rel="nofollow" href="https://api.nasa.gov/">
-              <img
-                src="assets/img/logo-NASA.svg"
-                alt="Logo to link to the NASA API REST page"
-              ></img>
-            </a>
-          </div>
+          <a rel="nofollow" href="https://api.nasa.gov/">
+            <img
+              src="assets/img/logo-NASA.svg"
+              alt="Logo to link to the NASA API REST page"
+            ></img>
+          </a>
         </li>
         <li>
-          <div>
-            <a rel="nofollow" href="https://twitter.com/opennasa?lang=en">
-              <img
-                src="assets/img/logo-twitter.svg"
-                alt="Logo to link to the Open NASA's Twitter page"
-              ></img>
-            </a>
-          </div>
+          <a rel="nofollow" href="https://twitter.com/opennasa?lang=en">
+            <img
+              src="assets/img/logo-twitter.svg"
+              alt="Logo to link to the Open NASA's Twitter page"
+            ></img>
+          </a>
         </li>
       </ul>
     </footer>
