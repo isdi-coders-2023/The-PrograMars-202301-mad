05fa388cb87738a8f9abc2dc7ac6941619ef477d
SemperFenix <dureivan@gmail.com>
2023-02-13 21:06:11 +0100

    Update import urls

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
+import { Header } from '../header/header';
+import { Footer } from '../footer/footer';
+import { AppRouter } from '../app.router/approuter';
 
-jest.mock('..'); //Faltaintroducir Header folder
-jest.mock('..'); //Falta introducir AppRouter folder
-jest.mock('..'); //Falta introducir Footer folder
+jest.mock('../header/header'); //Faltaintroducir Header folder
+jest.mock('../app.router/approuter'); //Falta introducir AppRouter folder
+jest.mock('../footer/footer'); //Falta introducir Footer folder
 
 describe('Given App component', () => {
   beforeEach(() => {
     // eslint-disable-next-line testing-library/no-render-in-setup
-    render(<Header></Header>);
+    render(
+      <Header>
+        <></>
+      </Header>
+    );
   });
   describe('When it is render', () => {
     test('Then it should call Header, AppRouter & Footer', () => {
