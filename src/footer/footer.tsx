192b4b5aa60c119a15102cc22bf15bef8b6f44a7
sfernandez <sfdezlop@gmail.com>
2023-02-14 20:18:46 +0100

    Update footer scss y tsx

--- src/footer/footer.tsx
+++ src/footer/footer.tsx
@@ -4,17 +4,19 @@ export function Footer() {
     <footer className="footer">
       <ul className="footer__ul">
         <div>
-          <li className="footer__img">
+          <li>
             <a rel="nofollow" href="https://twitter.com/ISDICoders">
               <img
+                className="footer__img"
                 src="assets/img/logo-twitter.svg"
                 alt="Logo to link to the ISDI Coders's Twitter page"
               ></img>
             </a>
           </li>
-          <li className="footer__img">
+          <li>
             <a rel="nofollow" href="https://isdicoders.com/">
               <img
+                className="footer__img"
                 src="assets/img/logo-ISDI.svg"
                 alt="Logo to link to the ISDI Coders's Twitter page"
               ></img>
@@ -25,17 +27,19 @@ export function Footer() {
           <p>ISDI coders 2023 - NASA API</p>
         </li>
         <div>
-          <li className="footer__img">
+          <li>
             <a rel="nofollow" href="https://api.nasa.gov/">
               <img
+                className="footer__img"
                 src="assets/img/logo-NASA.svg"
                 alt="Logo to link to the NASA API REST page"
               ></img>
             </a>
           </li>
-          <li className="footer__img">
+          <li>
             <a rel="nofollow" href="https://twitter.com/opennasa?lang=en">
               <img
+                className="footer__img"
                 src="assets/img/logo-twitter.svg"
                 alt="Logo to link to the Open NASA's Twitter page"
               ></img>
