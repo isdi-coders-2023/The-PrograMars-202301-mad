69e9473fc1f03631fcbc39980d65d22e69789ac5
Daniel <dursuaf@gmail.com>
2023-02-14 13:46:13 +0100

    Update header css

--- src/header/header.tsx
+++ src/header/header.tsx
@@ -6,11 +6,13 @@ export function Header({ children }: HeaderProps) {
   return (
     <header className="header">
       <img
-        src="assets/logo.png"
+        src="assets/img/logo.png"
         alt="Logo formed by a icon of a rover vehicle."
-        width="45px"
+        className="header__img"
       />
-      <h1 className="header__h1">PROGRA-MARS</h1>
+      <h1 className="header__h1" hidden>
+        PROGRA-MARS
+      </h1>
       {children}
     </header>
   );
