9af60bafb6549ca79eb3f5fbeda7c6ac65e6fc7e
Daniel <dursuaf@gmail.com>
2023-02-14 13:59:54 +0100

    Fix css problems

--- src/header/header.tsx
+++ src/header/header.tsx
@@ -10,9 +10,7 @@ export function Header({ children }: HeaderProps) {
         alt="Logo formed by a icon of a rover vehicle."
         className="header__img"
       />
-      <h1 className="header__h1" hidden>
-        PROGRA-MARS
-      </h1>
+      <h1 className="header__h1">PROGRA-MARS</h1>
       {children}
     </header>
   );
