f88cb9b394741bbf0c101a3e328b52f827ec9c17
Elena <ebromeo88@gmail.com>
2023-02-15 19:51:20 +0100

    Add import photosApiRepo

--- src/app.context/app.context.provider.tsx
+++ src/app.context/app.context.provider.tsx
@@ -1,9 +1,9 @@
 import { usePhotos } from '../hooks/use.photo.mars';
-// import { NasaApiRepo } from "../services/"; Iván
+import { PhotosApiRepo } from '../services/repository/nasa.api.repo';
 import { AppContext } from './app.context';
 
 export function AppContextProvider({ children }: { children: JSX.Element }) {
-  const repo = new NasaApiRepo();
+  const repo = new PhotosApiRepo();
   const context = usePhotos(repo);
 
   return <AppContext.Provider value={context}>{children}</AppContext.Provider>;
