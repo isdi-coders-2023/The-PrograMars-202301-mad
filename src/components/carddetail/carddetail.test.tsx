46bed23e8bf6832650cf666c08cca25530c58942
sfernandez <sfdezlop@gmail.com>
2023-02-16 18:30:22 +0100

    Add test describe for false value of isFavorite

--- src/components/carddetail/carddetail.test.tsx
+++ src/components/carddetail/carddetail.test.tsx
@@ -3,9 +3,21 @@ import { render, screen } from '@testing-library/react';
 import { MOCK_MARSPHOTOS } from '../../mocks/marsPhotos';
 
 describe('Given CardDetail component', () => {
-  describe('When it is render', () => {
+  describe('When it is render using index 0 of mock', () => {
     test('Then the screen it should show the text Camera', () => {
       render(<CardDetail info={MOCK_MARSPHOTOS[0]}></CardDetail>);
+      const element = screen.getByText(/#/i);
+      expect(element).toBeInTheDocument();
+    });
+  });
+});
+
+describe('Given a CardDetail component', () => {
+  describe('When it is render using index 0 of mock with property isFavorite changed to false', () => {
+    test('Then the screen it should show the text Camera', () => {
+      const mockTemp = { ...MOCK_MARSPHOTOS[0] };
+      mockTemp.isFavorite = false;
+      render(<CardDetail info={mockTemp}></CardDetail>);
       const element = screen.getByText(/#/i);
       expect(element).toBeInTheDocument();
     });
