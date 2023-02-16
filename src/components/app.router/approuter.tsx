c1b92603011af64eb5cd242235a02eaceb22133e
sfernandez <sfdezlop@gmail.com>
2023-02-16 11:13:42 +0100

    Solve conflicts with approuter

--- src/components/app.router/approuter.tsx
+++ src/components/app.router/approuter.tsx
@@ -2,22 +2,12 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { MenuOption } from '../app/App';
 import { lazy, Suspense } from 'react';
 
-<<<<<<< HEAD:src/app.router/approuter.tsx
-const Home = lazy(() => import('../pages/home'));
-const Gallery = lazy(() => import('../pages/gallery'));
-const NewItem = lazy(() => import('../pages/new.item'));
-const Favorites = lazy(() => import('../pages/favorites'));
-const About = lazy(() => import('../pages/about'));
-const Card = lazy(() => import('../pages/error'));
-const Error = lazy(() => import('../pages/error'));
-=======
 const Home = lazy(() => import('../../pages/homepage/homepage'));
 const Gallery = lazy(() => import('../../pages/gallery/gallery'));
 const NewItem = lazy(() => import('../../pages/new item/new.item'));
 const Favorites = lazy(() => import('../../pages/favorites/favorites'));
 const About = lazy(() => import('../../pages/about/about'));
 const Error = lazy(() => import('../../pages/error/error'));
->>>>>>> 917551ee47a122f658ba23b80e6b018c221db51f:src/components/app.router/approuter.tsx
 
 type AppRouterProps = {
   menuOptions: MenuOption[];
@@ -36,7 +26,6 @@ export function AppRouter({ menuOptions }: AppRouterProps) {
           element={<Favorites></Favorites>}
         ></Route>
         <Route path={menuOptions[4].path} element={<About></About>}></Route>
-        <Route path={menuOptions[5].path} element={<Card></Card>}></Route>
         <Route path={'/error'} element={<Error></Error>}></Route>
 
         <Route
