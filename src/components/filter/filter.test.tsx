88a5494a15b98a028003a84809fa1f8dcf4b222b
Daniel <dursuaf@gmail.com>
2023-02-16 10:24:42 +0100

    Update filter test

--- src/components/filter/filter.test.tsx
+++ src/components/filter/filter.test.tsx
@@ -5,7 +5,10 @@ describe('Given the component filter', () => {
   describe('When it is render', () => {
     test('Then it should print in the screen the radio buttons', () => {
       render(<Filter></Filter>);
-      const elements = screen.getAllByRole('radio');
+      const elements = [
+        screen.getAllByRole('radio');
+        screen.getByText(/Select camera/i)
+      ]
       expect(elements.length).toBe(7);
     });
   });
