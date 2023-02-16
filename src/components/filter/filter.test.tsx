87ad9f20ff035cd47a561e40bf2a245e8bc24bf5
Daniel <dursuaf@gmail.com>
2023-02-16 11:14:52 +0100

    Add new test

--- src/components/filter/filter.test.tsx
+++ src/components/filter/filter.test.tsx
@@ -5,11 +5,11 @@ describe('Given the component filter', () => {
   describe('When it is render', () => {
     test('Then it should print in the screen the radio buttons', () => {
       render(<Filter></Filter>);
-      const elements = [
-        screen.getAllByRole('radio');
-        screen.getByText(/Select camera/i)
-      ]
-      expect(elements.length).toBe(7);
+      const radioElement = screen.getAllByRole('radio');
+      const textElement = screen.getByRole('group');
+
+      expect(radioElement.length).toBe(7);
+      expect(textElement).toBeInTheDocument();
     });
   });
 });
