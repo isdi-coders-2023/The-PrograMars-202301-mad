398fc3ca7471914b95d1915da570e994fb8628eb
SemperFenix <dureivan@gmail.com>
2023-02-16 16:16:31 +0100

    Add card component

---
+++ src/components/card/card.tsx
@@ -0,0 +1,20 @@
+import { SyntheticEvent } from 'react';
+import { Photo } from '../../models/api.model';
+
+interface CardData {
+  info: Photo;
+}
+
+export function Card({ info }: CardData) {
+  const handleClick = (ev: SyntheticEvent) => {
+    console.log('clicked');
+  };
+
+  return (
+    <div onClick={handleClick}>
+      <img className="mars-photo" src={info.img_src} alt={info.earth_date} />
+      <p>Camera: {info.camera.name}</p>
+      <p>#{info.id}</p>
+    </div>
+  );
+}
