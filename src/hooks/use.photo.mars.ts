11c2a9e5f973e4f28a6b2480b4dac8b41c6808dd
Elena <ebromeo88@gmail.com>
2023-02-17 17:25:54 +0100

    Modify amend typing named

--- src/hooks/use.photo.mars.ts
+++ src/hooks/use.photo.mars.ts
@@ -1,17 +1,15 @@
-import { useCallback, useEffect } from 'react';
+import { useCallback, useReducer, useEffect } from 'react';
 import { photosReducer } from '../reducer/photos.reducer';
 import { NasaApiRepo } from '../services/repository/nasa.api.repo';
-import * as ac from '../reducer/photos.actions.creator';
+
+import { loadPhotosCreator } from '../reducer/photos.actions.creator';
+import { MarsPhotoStructure } from '../models/marsPhoto';
 
 export type UseMarsStructure = ReturnType<typeof usePhotos>;
+
 export function usePhotos(repo: NasaApiRepo) {
-  const initialState: RootObject = {
-    photos: [],
-  };
-
+  const initialState: MarsPhotoStructure[] = [];
   const [photos, dispatch] = useReducer(photosReducer, initialState);
-
-  console.log('PHOTOS: ', photos);
 
   const handlerError = (error: Error) => {
     console.log(error.message);
@@ -20,7 +18,7 @@ export function usePhotos(repo: NasaApiRepo) {
   const loadPhotos = useCallback(async () => {
     try {
       const photos = await repo.loadPhotos();
-      dispatch(ac.loadPhotosCreate(photos));
+      dispatch(loadPhotosCreator(photos));
     } catch (error) {
       handlerError(error as Error);
     }
