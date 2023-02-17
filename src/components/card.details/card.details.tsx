694d80b1e429b60febb7fb4717551284c8421639
SemperFenix <dureivan@gmail.com>
2023-02-17 16:00:33 +0100

    Add CardDetails component

---
+++ src/components/card.details/card.details.tsx
@@ -0,0 +1,29 @@
+import { MarsPhotoStructure } from '../../models/marsPhoto';
+
+type CardDetailsProps = {
+  info: MarsPhotoStructure;
+};
+
+export function CardDetails({ info }: CardDetailsProps) {
+  return (
+    <div>
+      <div>
+        <img src={info.img_src} alt="" />
+      </div>
+      <ul>
+        <li className="photo-property">Name</li>
+        <li className="photo-value">{info.favoriteName}</li>
+        <li className="photo-property">Date</li>
+        <li className="photo-value">{info.earth_date}</li>
+        <li className="photo-property">Rover's name</li>
+        <li className="photo-value">{info.rover_name}</li>
+        <li className="photo-property">Launch date</li>
+        <li className="photo-value">{info.rover_launch_date}</li>
+        <li className="photo-property">Land date</li>
+        <li className="photo-value">{info.rover_landing_date}</li>
+        <li className="photo-property">Camera</li>
+        <li className="photo-value">{info.camera_full_name}</li>
+      </ul>
+    </div>
+  );
+}
