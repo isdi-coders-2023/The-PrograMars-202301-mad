3d944810e5c198f90bf2d9309f55d9b19cbd1e12
SemperFenix <dureivan@gmail.com>
2023-02-18 20:31:30 +0100

    Update test for reducer

--- src/reducer/photos.reducer.test.ts
+++ src/reducer/photos.reducer.test.ts
@@ -48,6 +48,26 @@ describe('Given the photosReducer fuction', () => {
       },
     ],
   };
+  const mockloadPhotosCreator3 = {
+    type: photosCreator.actualCard,
+    payload: [
+      {
+        id: 1,
+        sol: 'test',
+        camera_id: 'test',
+        camera_name: 4,
+        camera_rover_id: 6,
+        camera_full_name: 'test',
+        img_src: 'test',
+        earth_date: 'test',
+        rover_id: 'test',
+        rover_name: 'test',
+        rover_landing_date: 'test',
+        rover_launch_date: 'test',
+        rover_status: 'test',
+      },
+    ],
+  };
 
   describe('When the action is not a switch case', () => {
     test('Then it should returned the initial state', async () => {
@@ -68,7 +88,7 @@ describe('Given the photosReducer fuction', () => {
 
   describe('When the action is actualCard', () => {
     test('Then it should return the state updating the actualPhoto property', async () => {
-      const photo = photosReducer(mockPhotos, mockloadPhotosCreator);
+      const photo = photosReducer(mockPhotos, mockloadPhotosCreator3);
       expect(photo).toEqual({
         ...mockPhotos,
         actualPhoto: mockloadPhotosCreator.payload,
