214fff99572c9174bd4f220edac41293242f1e86
Elena <ebromeo88@gmail.com>
2023-02-18 20:58:52 +0100

    Add page button file

--- src/components/page.button/page.button.tsx
+++ src/components/page.button/page.button.tsx
@@ -1,21 +1,41 @@
-import { useContext } from 'react';
-import { loadPhotosCreator } from '../../reducer/photos.actions.creator';
+import { useContext, useEffect } from 'react';
+import { AppContext } from '../../app.context/app.context';
+import './page.button.scss';
 
-export function PrevNextButtons() {
-  const { marsPhotos, loadPhotosCreator } = useContext();
-  const handlerButton = (changePage: number) => {
-      loadPhotosCreator();
-    },
-    [loadPhotosCreator];
+export function galleryButtons() {
+  const { photos, loadPhotos } = useContext(AppContext);
+
+  useEffect(() => {
+    loadPhotos();
+  }, [loadPhotos]);
+
+  const handlerButton = (page: number) => {
+    loadPhotos(page);
+  };
+  return (
+    <>
+      <div className="Gallery-next-prev-buttons">
+        <button
+          className="next-button"
+          onClick={() => {
+            handlerButton(+1);
+          }}
+        >
+          <img src="./public/assets/img/next-button.png" alt="Next button" />
+        </button>
+
+        <button
+          className="previous-button"
+          onClick={() => {
+            handlerButton(-1);
+          }}
+        >
+          <img
+            src="./public/assets/img/previous-button.png"
+            alt="Previous button"
+          />
+        </button>
+      </div>
+    </>
+  );
 }
-
-return (
-  <>
-    <div
-      className="next-button"
-      onClick={() => {
-        handlerButton(+1);
-      }}
-    ></div>
-  </>
-);
