705bbdc9e0e5f8e2e41dfd00701d1113344b2dcc
SemperFenix <dureivan@gmail.com>
2023-02-18 20:48:44 +0100

    Update test

--- src/components/card/card.test.tsx
+++ src/components/card/card.test.tsx
@@ -1,5 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { MemoryRouter as Router } from 'react-router-dom';
+import { PhotosContext } from '../../app.context/app.context';
+import { InitialStateStructure } from '../../hooks/use.photo.mars';
 import { Card } from './card';
 
 const mockPhoto = {
@@ -21,17 +24,50 @@ const mockPhoto = {
   favoriteName: 'test',
 };
 
+const mockState = {
+  photos: [],
+  actualPhoto: {
+    id: 1,
+    sol: 1,
+    camera_id: 1,
+    camera_name: 'test',
+    camera_rover_id: 1,
+    camera_full_name: '',
+    img_src:
+      'https://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG',
+    earth_date: '',
+    rover_id: 1,
+    rover_name: '',
+    rover_landing_date: '',
+    rover_launch_date: '',
+    rover_status: '',
+    apiOrigin: '',
+    isFavorite: false,
+    favoriteName: '',
+  },
+} as InitialStateStructure;
+
+const mockContext = {
+  state: mockState,
+  loadPhotos: jest.fn(),
+  actualCard: jest.fn(),
+};
+
 describe('Given the Card component', () => {
   beforeEach(() => {
-    jest.spyOn(console, 'log');
-
     // eslint-disable-next-line testing-library/no-render-in-setup
-    render(<Card info={mockPhoto}></Card>);
+    render(
+      <Router>
+        <PhotosContext.Provider value={mockContext}>
+          <Card info={mockPhoto}></Card>
+        </PhotosContext.Provider>
+      </Router>
+    );
   });
   describe('When called', () => {
     test('Then it should renderize a card component', () => {
       const element = screen.getByRole('img');
-      const element2 = screen.getByText(/FHAZ/i);
+      const element2 = screen.getByText(/test/i);
       expect(element).toBeInTheDocument();
       expect(element2).toBeInTheDocument();
     });
@@ -41,7 +77,7 @@ describe('Given the Card component', () => {
     test('Then it should call handleClick', () => {
       const element = screen.getByRole('img');
       userEvent.click(element);
-      expect(console.log).toHaveBeenCalled();
+      // Test incompleto, pendiente de testear si el click lleva al nuevo Path
     });
   });
 });
