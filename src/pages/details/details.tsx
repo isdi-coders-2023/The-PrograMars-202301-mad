3894039472c5bda85667733ce878839d3c0a113c
SemperFenix <dureivan@gmail.com>
2023-02-18 18:24:43 +0100

    Remove unnecessary code

--- src/pages/details/details.tsx
+++ src/pages/details/details.tsx
@@ -1,9 +1,4 @@
 import { Details } from '../../components/details/details';
-import { MarsPhotoStructure } from '../../models/marsPhoto';
-
-interface DetailProps {
-  info: MarsPhotoStructure;
-}
 
 export default function DetailsPage() {
   return <Details />;
