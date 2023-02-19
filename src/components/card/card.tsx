c34638c12b519d7d4952867a8388e2d7a390c494
Daniel <dursuaf@gmail.com>
2023-02-19 17:32:57 +0100

    Add favorite icon

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -14,26 +14,43 @@ export function Card({ info }: CardData) {
   const handleClick = (ev: SyntheticEvent) => {
     actualCard({ ...info });
   };
-
+  const handleClickFavorite = (ev: SyntheticEvent) => {
+    const element = document.querySelector('.fa-star') as HTMLElement;
+    element?.classList.toggle('fa-solid');
+    info.isFavorite = !info.isFavorite;
+  };
   return (
-    <Link to={'/details'}>
-      <div onClick={handleClick} className="card">
-        <img
-          className="card__photo"
-          src={info.img_src}
-          alt={info.earth_date}
-          onClick={handleClick}
-        />
-        <div className="card__info">
-          <p>
-            <span> Camera:</span> {info.camera_name}
-          </p>
-          <p>
-            <span>Photo number: </span>
-            {info.id}
-          </p>
-        </div>
-      </div>
-    </Link>
+    <>
+      <article className="card">
+        <i
+          className={
+            info?.isFavorite
+              ? 'fa-solid fa-star card__fav'
+              : 'fa-regular fa-star card__fav'
+          }
+          role="button"
+          onClick={handleClickFavorite}
+        ></i>
+        <Link to={'/details'}>
+          <div onClick={handleClick}>
+            <img
+              className="card__photo"
+              src={info.img_src}
+              alt={info.earth_date}
+              onClick={handleClick}
+            />
+            <div className="card__info">
+              <p>
+                <span> Camera:</span> {info.camera_name}
+              </p>
+              <p>
+                <span>Photo number: </span>
+                {info.id}
+              </p>
+            </div>
+          </div>
+        </Link>
+      </article>
+    </>
   );
 }
