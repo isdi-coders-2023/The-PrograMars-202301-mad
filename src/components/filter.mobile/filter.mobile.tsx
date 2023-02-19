4cabce30453c80ee46c079995c2356120519e60e
SemperFenix <dureivan@gmail.com>
2023-02-19 12:41:59 +0100

    Update tests for coverage

--- src/components/filter.mobile/filter.mobile.tsx
+++ src/components/filter.mobile/filter.mobile.tsx
@@ -18,12 +18,13 @@ export function FilterMobile() {
         htmlFor={'camera-select'}
         id={'camera-select'}
         className="filter__title"
+        key="filter"
       >
         Camera:
       </label>
       <select name="camera" id={'camera-select'} className="filter__options">
         {cams.map((item) => (
-          <option value={item} className="filter__option" key={item}>
+          <option value={item} className="filter__option" key={item + 'select'}>
             {item.toUpperCase()}
           </option>
         ))}
