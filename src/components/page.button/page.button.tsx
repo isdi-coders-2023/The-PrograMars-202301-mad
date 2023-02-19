bfd1d7c80eb70a8129f3f6675200b7a7b588ec6a
Elena <ebromeo88@gmail.com>
2023-02-19 19:35:53 +0100

    Modify pageButtpn

--- src/components/page.button/page.button.tsx
+++ src/components/page.button/page.button.tsx
@@ -9,18 +9,26 @@ export function PageButtons() {
     loadPhotos();
   }, [loadPhotos]);
 
-  const handlerClick = () => {
+  const handlerClickNext = () => {
+    state.actualPage = '' + (parseInt(state.actualPage) + 1);
+
+    loadPhotos();
+  };
+  const handlerClickPrev = () => {
+    if (state.actualPage !== '1') {
+      state.actualPage = '' + (parseInt(state.actualPage) - 1);
+    }
     loadPhotos();
   };
 
   return (
     <>
       <div className="Gallery-next-prev-buttons">
-        <button className="previous-button" onClick={handlerClick}>
+        <button className="previous-button" onClick={handlerClickPrev}>
           <img src="/assets/img/previous-button.png" alt="Previous button" />
         </button>
         <p>{state.actualPage}</p>
-        <button className="next-button" onClick={handlerClick}>
+        <button className="next-button" onClick={handlerClickNext}>
           <img src="/assets/img/next-button.png" alt="Next button" />
         </button>
       </div>
