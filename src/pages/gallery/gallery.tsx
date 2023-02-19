a11f0507a8b6ab9c839f5a57e3e553a3a781b2f8
Elena <ebromeo88@gmail.com>
2023-02-19 13:22:11 +0100

    Vincular con botones de paginado

--- src/pages/gallery/gallery.tsx
+++ src/pages/gallery/gallery.tsx
@@ -1,5 +1,12 @@
 import { Gallery } from '../../components/gallery/gallery';
+import { GalleryButtons } from '../../components/page.button/page.button';
 
 export default function GalleryPage() {
-  return <Gallery></Gallery>;
+  return (
+    <Gallery>
+      <div>
+        <GalleryButtons></GalleryButtons>
+      </div>
+    </Gallery>
+  );
 }
