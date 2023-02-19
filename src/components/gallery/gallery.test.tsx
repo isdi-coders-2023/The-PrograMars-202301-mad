774f51925d5e392f03f917423ecf4ed257b2683c
SemperFenix <dureivan@gmail.com>
2023-02-19 21:11:23 +0100

    Update test

--- src/components/gallery/gallery.test.tsx
+++ src/components/gallery/gallery.test.tsx
@@ -8,6 +8,15 @@ describe('Given Gallery component', () => {
     render(<Gallery></Gallery>);
   });
   describe('When it is rendering', () => {
+    test('Then it should be return a combobox', () => {
+      const element = screen.getByRole('combobox');
+      expect(element).toBeInTheDocument();
+      const element2 = screen.getByRole('main');
+      expect(element2).toBeInTheDocument();
+    });
+  });
+
+  describe('When it is rendering after call', () => {
     test('Then it should be return an image', async () => {
       act(async () => {
         const element = await screen.findAllByRole('img');
