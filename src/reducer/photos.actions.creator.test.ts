600052371a0cebc8aa61e11016fe459ebde67854
SemperFenix <dureivan@gmail.com>
2023-02-19 11:34:11 +0100

    Update test for coverage

--- src/reducer/photos.actions.creator.test.ts
+++ src/reducer/photos.actions.creator.test.ts
@@ -1,8 +1,8 @@
 import { photosCreator } from './photos.actions';
-import { loadPhotosCreator } from './photos.actions.creator';
+import { loadPhotosCreator, actualCardCreator } from './photos.actions.creator';
 
 describe('Given the photos actions creator', () => {
-  describe('When called the function', () => {
+  describe('When called the function load', () => {
     test('Then it should return an object with the proper type and payload', () => {
       const mockPhotoStructure = [
         {
@@ -26,6 +26,37 @@ describe('Given the photos actions creator', () => {
       ];
       const element = loadPhotosCreator(mockPhotoStructure);
       const result = { type: photosCreator.load, payload: mockPhotoStructure };
+      expect(element).toEqual(result);
+    });
+  });
+
+  describe('When called the function actualCard', () => {
+    test('Then it should return an object with the proper type and payload', () => {
+      const mockPhotoStructure = [
+        {
+          id: 1,
+          sol: 1,
+          camera_id: 3,
+          camera_name: 'test',
+          camera_rover_id: 6,
+          camera_full_name: 'test',
+          img_src: 'test',
+          earth_date: 'test',
+          rover_id: 1,
+          rover_name: 'test',
+          rover_landing_date: 'test',
+          rover_launch_date: 'test',
+          rover_status: 'test',
+          apiOrigin: 'APIPublic',
+          isFavorite: false,
+          favoriteName: 'test',
+        },
+      ];
+      const element = actualCardCreator(mockPhotoStructure[0]);
+      const result = {
+        type: photosCreator.actualCard,
+        payload: mockPhotoStructure[0],
+      };
       expect(element).toEqual(result);
     });
   });
