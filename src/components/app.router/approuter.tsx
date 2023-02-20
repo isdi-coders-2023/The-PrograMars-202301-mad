9dda28cc31fd6c7c5a13de8149c0457221eacf24
Daniel <dursuaf@gmail.com>
2023-02-20 09:00:00 +0100

    Fix code smell

--- src/components/app.router/approuter.tsx
+++ src/components/app.router/approuter.tsx
@@ -6,6 +6,7 @@ const Home = lazy(() => import('../../pages/home/home'));
 const Gallery = lazy(() => import('../../pages/gallery/gallery'));
 const NewItem = lazy(() => import('../../pages/new.item/new.item'));
 const Favorites = lazy(() => import('../../pages/favorites/favorites'));
+const About = lazy(() => import('../../pages/about/about'));
 const Error = lazy(() => import('../../pages/error/error'));
 const Details = lazy(() => import('../../pages/details/details'));
 
@@ -24,6 +25,7 @@ export function AppRouter({ menuOptions }: AppRouterProps) {
           path={menuOptions[3].path}
           element={<Favorites></Favorites>}
         ></Route>
+        <Route path={menuOptions[4].path} element={<About></About>}></Route>
         <Route path={'/details'} element={<Details></Details>}></Route>
         <Route path={'/error'} element={<Error></Error>}></Route>
 
