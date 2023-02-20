95041d2b5aebdeb2b94beb6387396adfa2a3e8bc
SemperFenix <dureivan@gmail.com>
2023-02-20 18:13:49 +0100

    Add function test

--- src/services/repository/private.api.repo.test.ts
+++ src/services/repository/private.api.repo.test.ts
@@ -1,3 +1,4 @@
+import { mockPhoto } from '../../mocks/tests.mocks';
 import { PrivateApiRepo } from './private.api.repo';
 
 describe('Given the service Private repo class', () => {
@@ -10,6 +11,18 @@ describe('Given the service Private repo class', () => {
       expect(repo).toBeInstanceOf(PrivateApiRepo);
       const load = await repo.loadPrivatePhotos();
       expect(load).toEqual({ photos: [] });
+    });
+  });
+
+  describe('When call the create Item method', () => {
+    test('Then it should be able to call its method', async () => {
+      global.fetch = jest.fn().mockResolvedValue({
+        json: jest.fn().mockResolvedValue(mockPhoto),
+      });
+      const repo = new PrivateApiRepo();
+      expect(repo).toBeInstanceOf(PrivateApiRepo);
+      const load = await repo.createPrivatePhoto(mockPhoto);
+      expect(load).toEqual(mockPhoto);
     });
   });
 });
